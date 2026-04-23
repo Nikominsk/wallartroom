f8d52e6f01839daa58c19b8794242e1ef18a4977
Nikominsk <nniko.geuenich@gmail.com>
2026-04-23 22:38:41 +0200

    add page

---
+++ app/pages/privacy.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <h2>Datenschutzerklärung</h2>
+
+    Diese Anwendung wird ausschließlich privat genutzt.
+    Es werden keine personenbezogenen Daten von Dritten gespeichert oder verarbeitet.
+    Die Nutzung dient ausschließlich dem automatisierten Posten von Inhalten auf Pinterest.
+
+    <p>Kontakt: nniko.geuenich@gmail.com </p>
+  </section> 
+    
+</template> 
